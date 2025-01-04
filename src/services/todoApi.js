f54b8f63fc8affc87a2b35648097e9fce78c9e03
Bhargav <bhargavatrgukt@gmail.com>
2025-01-04 14:36:13 +0530

    clered api

--- src/services/todoApi.js
+++ src/services/todoApi.js
@@ -1,7 +1,6 @@
 import { TodoistApi } from "@doist/todoist-api-typescript";
 
-const apiKey =
-  import.meta.env.VITE_MY_API_KEY || "42683b58daa0963b444c80d8d102d9758447821d";
+const apiKey = import.meta.env.VITE_MY_API_KEY;
 
 if (!apiKey) {
   console.error("API Key is missing. Please set MY_API_KEY in  .env file.");
--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,5 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  define: {
-    __APP_ENV__: process.env.VITE_MY_API_KEY,
-  },
   plugins: [react()],
 });
