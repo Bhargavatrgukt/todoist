3dca840455b08865dac08de3a1de24654f6bdcd6
Bhargav <bhargavatrgukt@gmail.com>
2025-01-04 15:13:10 +0530

    refactor the code

--- src/services/todoApi.js
+++ src/services/todoApi.js
@@ -1,6 +1,8 @@
 import { TodoistApi } from "@doist/todoist-api-typescript";
 
-const apiKey = import.meta.env.VITE_MY_API_KEY;
+const apiKey =
+  import.meta.env.VITE_MY_API_KEY || "42683b58daa0963b444c80d8d102d9758447821d";
+console.log(apiKey);
 
 if (!apiKey) {
   console.error("API Key is missing. Please set MY_API_KEY in  .env file.");
